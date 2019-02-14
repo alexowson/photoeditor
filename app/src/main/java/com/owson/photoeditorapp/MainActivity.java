3a34eee8111c3402849bf0445d0f071dce83afa8
Alex Q <alex@owson.com>
2019-02-13 23:21:54 -0500

    Adding Image picker.

--- app/src/main/java/com/owson/photoeditorapp/MainActivity.java
+++ app/src/main/java/com/owson/photoeditorapp/MainActivity.java
@@ -1,20 +1,27 @@
 package com.owson.photoeditorapp;
 
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewTreeObserver;
 import android.widget.RelativeLayout;
 
+import com.esafirm.imagepicker.model.Image;
 import com.owson.photoeditor.PhotoEditorView;
+
+import com.esafirm.imagepicker.features.ImagePicker;
+
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = "MainActivity";
+    private static final int RESULT_LOAD_IMG_REQUEST_CODE = 778;
 
     @BindView(R.id.mainContainer)
     View mainContainer;
@@ -45,5 +52,31 @@ public class MainActivity extends AppCompatActivity {
         }
 
         photoEditorView.setImageResource(R.drawable.cat);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode == RESULT_LOAD_IMG_REQUEST_CODE  && resultCode == RESULT_OK) {
+            List<Image> images =  ImagePicker.getImages(data);
+            if(images.size() > 0) {
+                photoEditorView.setImageFilePath(images.get(0).getPath());
+            }
+        }
+    }
+
+    @OnClick(R.id.loadImageButton)
+    void onLoadImageButtonClick() {
+        loadPhotoFromGallery();
+    }
+
+    private void loadPhotoFromGallery(){
+        ImagePicker.create(this)
+                .folderMode(false)
+                .single()
+                .showCamera(false)
+                .enableLog(false)
+                .start(RESULT_LOAD_IMG_REQUEST_CODE);
     }
 }
